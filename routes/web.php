788274c5af3b8d04158da34962098115e7029af4
dev-ninja-07 <genuine5621@gmail.com>
2025-03-14 17:35:30 +0300

    Modified the users table migration file to include new fields and updated web routes for better organization and functionality.

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,9 @@ use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/dashboard', function () {
+Route::get('{path?}', function () {
     return view('dashboard.main');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->where('path', '|dashboard')->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
