a348075f29982ad6d6dfdc469f1b274e9488aaeb
dev-ninja-07 <genuine5621@gmail.com>
2025-03-15 18:07:16 +0300

    `Refactored side menu items to use hasrole directive for authorization`

--- resources/views/dashboard/shard/sideBar.blade.php
+++ resources/views/dashboard/shard/sideBar.blade.php
@@ -18,7 +18,7 @@
                 </div>
                 <div class="user-info">
                     <h4 class="font-weight-semibold mt-3 mb-0">{{ auth()->user()->name }}</h4>
-                    <span class="mb-0 text-muted">    {{ auth()->user()->roles->first()->name }}</span>
+                    <span class="mb-0 text-muted"> {{ auth()->user()->roles->first()->name }}</span>
                 </div>
             </div>
         </div>
@@ -34,38 +34,40 @@
                     </svg><span class="side-menu__label">Main</span><span
                         class="badge badge-success side-badge">1</span></a>
             </li>
-            <li class="side-item side-item-category"> Authorization </li>
-            <li class="slide">
-                <a class="side-menu__item" href="{{ route('user.index') }}">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24">
-                        <path d="M0 0h24v24H0V0z" fill="none" />
-                        <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4z" opacity=".3" />
-                        <path
-                            d="M12 14c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4zm-6 4c.22-.72 3.31-2 6-2 2.7 0 5.8 1.29 6 2H6zm6-6c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0-6c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2z" />
-                    </svg>
-                    <span class="side-menu__label">Users</span>
-                </a>
-            </li>
-            <li class="slide">
-                <a class="side-menu__item" href="{{ route('role.index') }}"><svg xmlns="http://www.w3.org/2000/svg"
-                        class="side-menu__icon" viewBox="0 0 24 24">
-                        <path d="M0 0h24v24H0V0z" fill="none" />
-                        <path
-                            d="M12 4c-4.42 0-8 3.58-8 8s3.58 8 8 8 8-3.58 8-8-3.58-8-8-8zm4.5 11h-9v-2h9v2zm0-4h-9V9h9v2z"
-                            opacity=".3" />
-                        <path
-                            d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm4.5-11h-9v2h9V9zm0 4h-9v2h9v-2z" />
-                    </svg><span class="side-menu__label">Roles</span></a>
-            </li>
-            <li class="slide">
-                <a class="side-menu__item" href="{{ route('permission.index') }}"><svg
-                        xmlns="http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24">
-                        <path d="M0 0h24v24H0V0z" fill="none" />
-                        <path d="M12 4c-1.7 0-3 1.3-3 3v2h6V7c0-1.7-1.3-3-3-3zm5 6H7v8h10V10z" opacity=".3" />
-                        <path
-                            d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zM9 6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9V6zm9 14H6V10h12v10zm-6-3c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z" />
-                    </svg><span class="side-menu__label">Permissions</span></a>
-            </li>
+            @hasrole('super-admin')
+                <li class="side-item side-item-category"> Authorization </li>
+                <li class="slide">
+                    <a class="side-menu__item" href="{{ route('user.index') }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24">
+                            <path d="M0 0h24v24H0V0z" fill="none" />
+                            <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4z" opacity=".3" />
+                            <path
+                                d="M12 14c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4zm-6 4c.22-.72 3.31-2 6-2 2.7 0 5.8 1.29 6 2H6zm6-6c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0-6c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2z" />
+                        </svg>
+                        <span class="side-menu__label">Users</span>
+                    </a>
+                </li>
+                <li class="slide">
+                    <a class="side-menu__item" href="{{ route('role.index') }}"><svg xmlns="http://www.w3.org/2000/svg"
+                            class="side-menu__icon" viewBox="0 0 24 24">
+                            <path d="M0 0h24v24H0V0z" fill="none" />
+                            <path
+                                d="M12 4c-4.42 0-8 3.58-8 8s3.58 8 8 8 8-3.58 8-8-3.58-8-8-8zm4.5 11h-9v-2h9v2zm0-4h-9V9h9v2z"
+                                opacity=".3" />
+                            <path
+                                d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm4.5-11h-9v2h9V9zm0 4h-9v2h9v-2z" />
+                        </svg><span class="side-menu__label">Roles</span></a>
+                </li>
+                <li class="slide">
+                    <a class="side-menu__item" href="{{ route('permission.index') }}"><svg
+                            xmlns="http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24">
+                            <path d="M0 0h24v24H0V0z" fill="none" />
+                            <path d="M12 4c-1.7 0-3 1.3-3 3v2h6V7c0-1.7-1.3-3-3-3zm5 6H7v8h10V10z" opacity=".3" />
+                            <path
+                                d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zM9 6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9V6zm9 14H6V10h12v10zm-6-3c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z" />
+                        </svg><span class="side-menu__label">Permissions</span></a>
+                </li>
+            @endhasrole
             <li class="side-item side-item-category"> Management </li>
             <li class="slide">
                 <a class="side-menu__item" href="#"><svg xmlns="http://www.w3.org/2000/svg"
